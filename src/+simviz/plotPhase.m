640ca67d046e5af511773cefe906f5148379b888
Sara Gomez-Ramirez <102175716+gsara798@users.noreply.github.com>
2026-09-02 14:03:53 -0400

    Polish phase-map styling

--- src/+simviz/plotPhase.m
+++ src/+simviz/plotPhase.m
@@ -2,13 +2,25 @@ function fig = plotPhase(sample, options)
 %PLOTPHASE Plot complex phase using the shared blue-white-red palette.
 arguments
     sample (1,1) struct
-    options.Title (1,1) string = "Wavefield phase"
     options.Visible (1,1) string = "off"
 end
-[x,z,U,~,description] = simviz.displayPlane(sample);
+
+[x,z,U,~,~] = simviz.displayPlane(sample);
 fig = figure("Visible",char(options.Visible),"Color","w","Position",[100 100 760 620]);
-ax = axes(fig); imagesc(ax,x,z,angle(U)); axis(ax,"image"); set(ax,"YDir","normal");
-xlabel(ax,"x [mm]"); ylabel(ax,"z [mm]"); title(ax,options.Title + " | " + description);
-clim(ax,[-pi pi]); cb=colorbar(ax); ylabel(cb,"Phase [rad]");
-colormap(ax,simviz.divergingColormap(256)); simviz.applyFigureStyle(fig);
+ax = axes(fig);
+imagesc(ax,x,z,angle(U));
+axis(ax,"image");
+set(ax,"YDir","normal");
+xlabel(ax,"x [mm]");
+ylabel(ax,"z [mm]");
+clim(ax,[-pi pi]);
+colormap(ax,simviz.divergingColormap(256));
+
+cb = colorbar(ax);
+ylabel(cb,"Phase [rad]");
+cb.Ticks = [-pi, -pi/2, 0, pi/2, pi];
+cb.TickLabels = {'-\pi','-\pi/2','0','\pi/2','\pi'};
+cb.TickLabelInterpreter = "tex";
+
+simviz.applyFigureStyle(fig);
 end
