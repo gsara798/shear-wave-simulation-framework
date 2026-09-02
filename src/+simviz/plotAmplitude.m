dbd050b73d320b6090c60acf32649f49448a97b2
Sara Gomez-Ramirez <102175716+gsara798@users.noreply.github.com>
2026-09-02 14:04:19 -0400

    Use purple-to-yellow amplitude colormap

--- src/+simviz/plotAmplitude.m
+++ src/+simviz/plotAmplitude.m
@@ -2,13 +2,28 @@ function fig = plotAmplitude(sample, options)
 %PLOTAMPLITUDE Plot the wavefield amplitude.
 arguments
     sample (1,1) struct
-    options.Title (1,1) string = "Wavefield amplitude"
     options.Visible (1,1) string = "off"
 end
-[x,z,U,~,description] = simviz.displayPlane(sample);
+
+[x,z,U,~,~] = simviz.displayPlane(sample);
+values = abs(U);
+maximum = max(values,[],"all");
+if ~(isfinite(maximum) && maximum > 0)
+    maximum = 1;
+end
+
 fig = figure("Visible",char(options.Visible),"Color","w","Position",[100 100 760 620]);
-ax = axes(fig); imagesc(ax,x,z,abs(U)); axis(ax,"image"); set(ax,"YDir","normal");
-xlabel(ax,"x [mm]"); ylabel(ax,"z [mm]"); title(ax,options.Title + " | " + description);
-cb=colorbar(ax); ylabel(cb,"|U|");
-colormap(ax,simviz.sequentialColormap("teal",256)); simviz.applyFigureStyle(fig);
+ax = axes(fig);
+imagesc(ax,x,z,values);
+axis(ax,"image");
+set(ax,"YDir","normal");
+xlabel(ax,"x [mm]");
+ylabel(ax,"z [mm]");
+clim(ax,[0 maximum]);
+colormap(ax,simviz.sequentialColormap("blue",256,EndRole="yellow"));
+
+cb = colorbar(ax);
+ylabel(cb,"|U|");
+
+simviz.applyFigureStyle(fig);
 end
